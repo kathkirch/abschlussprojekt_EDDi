6c7e67fd1adacdbe58d00bdf6b6c39f41e71abec
lisah <lisa.heiler@edu.fh-joanneum.at>
2021-05-06 11:03:40 +0200

    activity_eintrag__stuhl created

---
+++ app/src/main/java/com/example/abschlussprojekt_eddi/Eintrag_Stuhl.java
@@ -0,0 +1,14 @@
+package com.example.abschlussprojekt_eddi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Eintrag_Stuhl extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eintrag__stuhl);
+    }
+}
